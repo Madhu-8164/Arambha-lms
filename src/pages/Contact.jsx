91d07618d305badbfc1f7bc3285e3e95d14909c8
Madhu-8164 <madhu.sn8164@gmail.com>
2026-02-24 22:40:36 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -53,7 +53,8 @@ const Contact = () => {
                                             Email Us
                                         </p>
                                         <p className="text-blue-800">
-                                            admissions@arambha.com
+                                            arambhaskilldesignsolutions@gmail.com
+9108032103
                                         </p>
                                     </div>
                                 </div>
@@ -67,7 +68,7 @@ const Contact = () => {
                                             Call Us
                                         </p>
                                         <p className="text-blue-800">
-                                            +91 98765 43210
+                                            +91 9108032103
                                         </p>
                                     </div>
                                 </div>
